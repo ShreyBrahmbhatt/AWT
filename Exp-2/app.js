b4a4ee242b0732124650a8d27ed7eb57c103a5ba
Lynx <shrey.brahmbhatt1111@gmail.com>
2026-02-06 14:55:26 +0530

    Clean repository structure: isolate Exp-2 Node project

---
+++ Exp-2/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+let tasks = [];
+
+// Home page
+app.get("/", (req, res) => {
+  res.render("index", { tasks });
+});
+
+// Add task
+app.post("/add", (req, res) => {
+  const task = req.body.task;
+  if (task) tasks.push(task);
+  res.redirect("/");
+});
+
+// Delete task
+app.post("/delete", (req, res) => {
+  const index = req.body.index;
+  tasks.splice(index, 1);
+  res.redirect("/");
+});
+
+app.listen(3000, () => {
+  console.log("Task Manager running on http://localhost:3000");
+});
